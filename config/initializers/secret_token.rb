206e0052d37d233cfd02955e8b778823a18f0edd
ichabodcole <ichabodcole@gmail.com>
2013-03-11 11:46:32 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cards::Application.config.secret_token = '36b6d48a6322db5215d57b32ee8eb83fa3d9f709100a263cd89ba5ef93ddb09c514bd91b42ff90bf5b04ca4d68601179dbf26dce6a8f85e4466fa1da2a05eb37'
